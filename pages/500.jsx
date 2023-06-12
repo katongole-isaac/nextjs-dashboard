9f9d1ddedeb9258730fdd5ee6df0b617e3da4c83
katongole isaac <katongolelsaac78@gmail.com>
2023-06-12 18:22:09 +0300

    linter configs updated

--- pages/500.jsx
+++ pages/500.jsx
@@ -4,11 +4,13 @@
  * 
  */
 
+import Link from "next/link";
+
 export default function custom500() {
   return (
     <div className="container-fluid vh-100 vw-100 d-flex justify-content-center align-items-center flex-column ">
       <p> 500 | Unexpected Error occured </p>
-      <a href="/"> &larr; Back to Dashboard </a>
+      <Link href="/"> &larr; Back to Dashboard </Link>
     </div>
   );
 }
